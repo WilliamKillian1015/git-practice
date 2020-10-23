f5fa2f4fb50aa691f3b3ed2fbe2268cad2b9ae96
William Killian <tj_killian@yahoo.com>
2020-10-23 17:23:26 -0400

    React Router

---
+++ src/components/MainComponent.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react';
+import Directory from './DirectoryComponent';
+import CampsiteInfo from './CampsiteInfoComponent';
+import Header from './HeaderComponent';
+import Footer from './FooterCompnent';
+import Home from './HomeCompnent';
+import { Switch, Route, Redirect, Router } from 'react-router-dom';
+import { CAMPSITES } from '../shared/campsites';
+
+class Main extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            campsites: CAMPSITES,
+        };
+    }
+
+    render() {
+
+        const HomePage= () => {
+            return (
+                <Home />
+            );
+        }
+        return (
+            <div>
+                <Header />
+                <Switch>
+                    <Route path='/home' component={HomePage} />
+                    <Route exact path='/directory' render={() => <Directory campsites={this.state.campsites} />} />
+                    <Redirect to ='/home' />
+                </Switch>
+                <Footer />
+            </div>
+        );
+    };
+}
+
+export default Main;
